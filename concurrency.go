df8fbf8070671c994dcce510ae5e4a9977d85a9c
unknown <slovojoe33@gmail.com>
2023-01-17 18:17:09 +0300

    Channels as signals

--- concurrency.go
+++ concurrency.go
@@ -5,10 +5,7 @@ import (
 	"os"
 )
 
-//imagine 2 functions
-func greet(){
-    fmt.Println(" General Kenobi")
-}
+//1.imagine 2 functions
 
 //Stores data to a file
 func storeData(storableText string, fileName string){
@@ -33,8 +30,53 @@ func storeData(storableText string, fileName string){
     }
 }
 
+//Repeatedly writes data to a file
+func repeatedWrite(lines int, fileName string, c chan int){
+
+    //Takes a number of lines and loops upto them
+    //Calls the storeData function for each iteration
+    
+    for i:=1; i<lines; i++{
+        text :=fmt.Sprintf("For %v Mordor\n",i)
+        storeData(text,fileName)
+    }
+
+    fmt.Printf("--Done storing %v lines of text --\n", lines)
+
+    //3)
+    //This function is going to be a goroutine. 
+    //To ensure it's completion before stoppin the program we add a channel as its parameter:  c chan int
+    //To use the channel: Call the channel followed by a (go specific) back arrow operator symbolizing the direction of data flow followed by any value matching the channel type
+
+    c<-1
+}
 func main(){
 
-    storeData("Someone on Facebook called Kratos, 'Keratosis'"," Funny typos")
+   // storeData("Someone on Facebook called Kratos, 'Keratosis'"," Funny typos")
 
+   //Create a channel and define its datatype
+   channel:=make(chan int)
+
+   // 1)
+   //Writing 5500 lines to a file might take a while especially when running multiple write operations
+   //Instead of waiting for each operation to finish before starting the next we can use goroutines to run them concurrently
+   //a goroutine is defined by adding the keyword "go" before a function
+
+            // go repeatedWrite(5500,"Middle Earth")   //Before channels
+            // go repeatedWrite(5500,"Middle Mars")    //Before channels
+
+    //2)
+    //By default goroutines start the next line of code before the routine is finished. If there is no more code the program stops
+    //In the above example code execution stops immediately after starting the second routine (Before it's fully complete)
+    //To prevent the above, channels can be used as defined by "channel:=make(chan int)"
+    //After defining a channel pass it to the function as a parameter
+        go repeatedWrite(5500,"Middle Earth",channel)   
+        go repeatedWrite(5500,"Middle Mars",channel)   
+    
+    //4
+    //Since the routine here have a channel param, go will start them simultaneously but wont close the progrum until data is received from the channel
+    //To fetch data from a channel use the back arrow operator to denote data flow direction
+    //For each routine, call the channel individually
+    <- channel
+    <- channel
 }
